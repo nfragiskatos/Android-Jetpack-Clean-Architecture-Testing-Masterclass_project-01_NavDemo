1a8f8c0a9647b83467831c92ac4a32e4c6af0af8
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2021-04-17 11:36:15 -0700

    - Quick Data Binding setup for activity_main.xml and MainActivity.kt

--- app/src/main/java/com/nfragiskatos/navdemo/MainActivity.kt
+++ app/src/main/java/com/nfragiskatos/navdemo/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.nfragiskatos.navdemo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.nfragiskatos.navdemo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
     }
 }
