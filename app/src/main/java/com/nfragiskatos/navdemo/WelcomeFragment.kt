527e747e884d7e2f3e69a474ad3a6809d229db01
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2021-04-18 09:19:17 -0700

    - Created WelcomeFragment.kt and fragment_welcome.xml - fragment_welcome.xml contains a 3 TextViews: a welcome banner, one to display user's name, and one to display the email. Also contains a "View Terms" button - Added fragment_welcome.xml as a navigation destination and set onClickListener for the "Submit" button in EnterEmailFragment.kt to navigate to WelcomeFragment.kt - Also had to setup Data Binding for fragment_enter_email.xml + EnterEmailFragment.kt

--- app/src/main/java/com/nfragiskatos/navdemo/WelcomeFragment.kt
+++ app/src/main/java/com/nfragiskatos/navdemo/WelcomeFragment.kt
@@ -5,9 +5,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
-import androidx.navigation.findNavController
-import com.nfragiskatos.navdemo.databinding.FragmentEnterEmailBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -16,14 +13,13 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [EnterEmailFragment.newInstance] factory method to
+ * Use the [WelcomeFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class EnterEmailFragment : Fragment() {
+class WelcomeFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
-    private lateinit var binding: FragmentEnterEmailBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,12 +33,8 @@ class EnterEmailFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = DataBindingUtil.inflate(layoutInflater, R.layout.fragment_enter_email, container, false)
-        binding.enterEmailSubmit.setOnClickListener {
-            it.findNavController().navigate(R.id.action_enterEmailFragment_to_welcomeFragment)
-        }
-
-        return binding.root
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_welcome, container, false)
     }
 
     companion object {
@@ -52,12 +44,12 @@ class EnterEmailFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment EnterEmailFragment.
+         * @return A new instance of fragment WelcomeFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            EnterEmailFragment().apply {
+            WelcomeFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
