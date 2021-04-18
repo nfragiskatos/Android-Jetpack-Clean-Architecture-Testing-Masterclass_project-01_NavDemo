c5cd3673a3d19585f602bf2f503473e1b7b99da1
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2021-04-18 08:41:28 -0700

    - Modified fragment_home.xml to have a "Sign Up" button and a "Terms" button - Created TermsFragment.kt and fragment_terms.xml - Added fragment_terms.xml as a navigation destination and set onClickListener for the "Terms" button in HomeFragment.kt to navigate to TermsFragment.kt

--- app/src/main/java/com/nfragiskatos/navdemo/TermsFragment.kt
+++ app/src/main/java/com/nfragiskatos/navdemo/TermsFragment.kt
@@ -1,13 +1,10 @@
 package com.nfragiskatos.navdemo
 
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
-import androidx.fragment.app.Fragment
-import androidx.navigation.findNavController
-import com.nfragiskatos.navdemo.databinding.FragmentHomeBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -16,14 +13,13 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [HomeFragment.newInstance] factory method to
+ * Use the [TermsFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class HomeFragment : Fragment() {
+class TermsFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
-    private lateinit var binding: FragmentHomeBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,19 +33,8 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false)
-        binding.homeViewTerms.setOnClickListener {
-            it.findNavController().navigate(R.id.action_homeFragment_to_termsFragment)
-        }
-//        binding.button.setOnClickListener {
-//            if (!TextUtils.isEmpty(binding.editText.text.toString())) {
-//                val bundle = bundleOf("user_input" to binding.editText.text.toString())
-//                it.findNavController().navigate(R.id.action_homeFragment_to_secondFragment, bundle)
-//            } else {
-//                Toast.makeText(activity, "Please Enter Name", Toast.LENGTH_LONG).show()
-//            }
-//        }
-        return binding.root
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_terms, container, false)
     }
 
     companion object {
@@ -59,12 +44,12 @@ class HomeFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment HomeFragment.
+         * @return A new instance of fragment TermsFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            HomeFragment().apply {
+            TermsFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
